b0d8eee250213328d84ca328418ad71f16fc66c5
NoNiName <64843882+NoNiName@users.noreply.github.com>
2022-03-18 12:22:27 +0700

    Update Serverhop.lua

--- Serverhop.lua
+++ Serverhop.lua
@@ -50,12 +50,70 @@ function TPReturner()
         end
     end
 end
+
+function TPReturnerLess()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(2) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            -- delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    -- writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                end)
+                wait(1)
+            end
+        end
+    end
+end
+
 function functeleport:Teleport() 
     while wait() do
         pcall(function()
             TPReturner()
             if foundAnything ~= "" then
                 TPReturner()
+            end
+        end)
+    end
+end
+
+function functeleport:TeleportLess() 
+    while wait() do
+        pcall(function()
+            TPReturnerLess()
+            if foundAnything ~= "" then
+                TPReturnerLess()
             end
         end)
     end
